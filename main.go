3600831122d035e69a92ee6c3dfc152cf2544a4f
Alex Peebles <alex_peebles@outlook.com>
2024-11-05 22:35:31 +0000

    fix: adding apigw

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/apigatewayv2"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/lambda"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -19,6 +20,14 @@ func createRSVPResources(ctx *pulumi.Context, guestTableName string) (*lambda.Fu
 		return nil, err
 	}
 	return rsvpLambda, err
+}
+
+func createApiGateway(ctx *pulumi.Context) (*apigatewayv2.Api, error) {
+	apiGway, err := createApiGatewayComponents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return apiGway, err
 }
 
 func main() {
@@ -45,6 +54,11 @@ func main() {
 		if err != nil {
 			return err
 		}
+		apiGateway, err = createApiGateway(ctx)
+		if err != nil {
+			return err
+		}
+		ctx.Export("api-url", apiGateway.Url)
 		return nil
 	})
 }
--- rsvp/main.go
+++ rsvp/main.go
@@ -2,27 +2,96 @@ package main
 
 import (
 	"context"
+	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-type userModelAuth struct {
-	guestName string `dynamodbav:"guestName"`
-	authToken string `dynamodbav:"authToken"`
-
-	hasPlusOne bool `dynamodbav:"hasPlusOne"`
-	isDayGuest bool `dynamodbav:"isDayGuest"`
+type Guest struct {
+	guestName    string                 `dynamodbav:"guestName"`
+	hasPlusOne   bool                   `dynamodbav:"hasPlusOne"`
+	isDayGuest   bool                   `dynamodbav:"isDayGuest"`
+	rsvpResponse map[string]interface{} `dynamodbav:"rsvpResponse"`
 }
 
 var dynamoDbObj *dynamodb.Client
 var tableName string
 
-func handleRequest() (string, error) {
-	return "Hello there my fried friend", nil
+func updateGuestRsvpResponse(ctx context.Context, guest Guest) (map[string]map[string]interface{}, error) {
+	var err error
+	var response *dynamodb.UpdateItemOutput
+	var attributeMap map[string]map[string]interface{}
+	update := expression.Set(expression.Name("rsvpResponse.attending"), expression.Value(guest.rsvpResponse["attending"]))
+	update.Set(expression.Name("rsvpResponse.songRequest"), expression.Value(guest.rsvpResponse["songRequest"]))
+	update.Set(expression.Name("rsvpResponse.dietaryReqs"), expression.Value(guest.rsvpResponse["dietaryReqs"]))
+	if guest.hasPlusOne {
+		update.Set(expression.Name("rsvpResponse.plusOneName"), expression.Value(guest.rsvpResponse["plusOneName"]))
+	}
+	expr, err := expression.NewBuilder().WithUpdate(update).Build()
+	if err != nil {
+		log.Printf("Couldn't build expression for update. Here's why: %v\n", err)
+	} else {
+		response, err = dynamoDbObj.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+			TableName:                 aws.String(tableName),
+			Key:                       guest.GetKey(),
+			ExpressionAttributeNames:  expr.Names(),
+			ExpressionAttributeValues: expr.Values(),
+			UpdateExpression:          expr.Update(),
+			ReturnValues:              types.ReturnValueUpdatedNew,
+		})
+		if err != nil {
+			log.Printf("Could not update guest %v RSVP response. Here's why: %v", guest.guestName, err)
+		} else {
+			err = attributevalue.UnmarshalMap(response.Attributes, &attributeMap)
+			if err != nil {
+				log.Printf("Cloud not unmarshall update response. Here's why %v\n", err)
+			}
+		}
+	}
+	return attributeMap, err
+}
 
+func getGuestDetails(ctx context.Context, guestName string) (Guest, error) {
+	guest := Guest{guestName: guestName}
+	response, err := dynamoDbObj.GetItem(ctx, &dynamodb.GetItemInput{
+		Key: guest.GetKey(), TableName: aws.String(tableName),
+	})
+	if err != nil {
+		log.Printf("Could not get info about %v. Here's why: %v\n", guestName, err)
+	} else {
+		err = attributevalue.UnmarshalMap(response.Item, &guest)
+		if err != nil {
+			log.Printf("Could not unmarshal response. Here's why: %v\n", err)
+		}
+	}
+	return guest, err
+
+}
+
+func (guest Guest) GetKey() map[string]types.AttributeValue {
+	guestName, err := attributevalue.Marshal(guest.guestName)
+	if err != nil {
+		panic(err)
+	}
+	return map[string]types.AttributeValue{"guestName": guestName}
+}
+
+func handleRequest(ctx context.Context) error {
+	guest, err := getGuestDetails(ctx, guestName)
+	if err != nil {
+		return err
+	}
+	_, err = updateGuestRsvpResponse(ctx, guest)
+	if err != nil {
+		return err
+	}
 }
 
 func main() {
